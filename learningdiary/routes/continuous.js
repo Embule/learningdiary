ce88b445dc6b7a30292680839d1a47d816694d0d
EmilyK <koskinen.emily@gmail.com>
2019-11-01 13:13:00 +0200

    eka branch luotu

--- learningdiary/routes/continuous.js
+++ learningdiary/routes/continuous.js
@@ -27,23 +27,25 @@ router.route('/topics').get((req, res) => {
     res.status(201);
     res.json(np);
 })
-router.route('/topics/:id').get((req, res) => {
+
+//idnamella hakeminen?
+router.route('/topics/:idName').get((req, res) => {
     console.log("Yksittäisen hakeminen");
     console.dir(req.params);
     //  res.send(req.params.id);
     for (let p of topics) {
-        if (p.id == req.params.id) {
+        if (p.idName == req.params.idName) {
             res.json(p)
             return;
         }
     }
     res.json({ msg: "Ei löydy" });
 }).delete((req, res) => {
-    console.log("Delete: " + req.params.id);
+    console.log("Delete: " + req.params.idName);
     for (let i=0; i<topics.length; i++) {
-        if (people[i].id == req.params.id) {
+        if (people[i].idName == req.params.idName) {
             topics.splice(i, 1);
-            res.json({msg:"deleted: " + req.params.id})
+            res.json({msg:"deleted: " + req.params.idName})
             saveTopics();
             return;
         }
